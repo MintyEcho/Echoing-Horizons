710c545fdaf29d5e55e0ea038b3d44a3961a1ef7
Echo <167474735+MintyEcho@users.noreply.github.com>
2026-03-11 22:32:55 +0200

    OKAY SO THIS IS REAL SETUP
    REAL SETUP THIS TIME
    ALL SERIOUS STUFF,
    NEXT TIME ILL HOP ON STRAIGHT TO WORK LETS GO

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<!-- mm svelte migration -->
+<!-- alright so i know what i just did im sorry i will be editing that in the future -->
+<!-- 10/3 i am utterly confused, i do not know how to use svelte, no idea what to do next. -->
+<!-- 10/3 please god help me i a -->
+
+<h1>CURRENT INTERESTS!!!!!111!!!1!111!</h1>
+<ul>
+  <li>Umineko when they cry</li>
+  <li>Sheena Ringo</li>
+  <li>Danganronpa</li>
+  <li>Persona 4 Golden</li>
+  <li>Bomb Rush Cyberfunk (and all grind fiction to be exact)</li>
+  <li>Pseudoregalia</li>
+</ul>
+
+<p>this will be deleted, or added in an easter egg via a secret button.</p>
+<p>for measure i will be making a specific design for each page.</p>
+<p>i need to find a way to integrate backend.</p>
+<p>i need an inclusive design in the main page which will be a mix probably.</p>
+<p>aaand i need to add fun mechanics via backend and js (it will be torture)</p>
