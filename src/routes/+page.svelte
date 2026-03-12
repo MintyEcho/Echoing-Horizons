09fb3b434ae3ce0732b858eb96eb133e18cfd05e
Echo <167474735+MintyEcho@users.noreply.github.com>
2026-03-12 23:03:20 +0200

    added things i forgot to add
    dumbass me forgot to install tailwind. anyways installed it and done almost nothing. byeee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
-<!-- mm svelte migration -->
-<!-- alright so i know what i just did im sorry i will be editing that in the future -->
+<!-- mm svelte migration
+<-- alright so i know what i just did im sorry i will be editing that in the future -->
 <!-- 10/3 i am utterly confused, i do not know how to use svelte, no idea what to do next. -->
 <!-- 10/3 please god help me i a -->
 
-<h1>CURRENT INTERESTS!!!!!111!!!1!111!</h1>
+<!-- <h1>CURRENT INTERESTS!!!!!111!!!1!111!</h1>
 <ul>
   <li>Umineko when they cry</li>
   <li>Sheena Ringo</li>
@@ -17,4 +17,18 @@
 <p>for measure i will be making a specific design for each page.</p>
 <p>i need to find a way to integrate backend.</p>
 <p>i need an inclusive design in the main page which will be a mix probably.</p>
-<p>aaand i need to add fun mechanics via backend and js (it will be torture)</p>
+<p>aaand i need to add fun mechanics via backend and js (it will be torture)</p>  -->
+<!-- Typescript -->
+<script>
+
+</script>
+
+<!-- HTML-->
+  <h1>Welcome to echo's personal dumpsterfire</h1>
+
+  <p>i'm so echo like dam im so minty mint mint mint mint</p>
+  <p>Echo's mint of mint dome</p>
+<!-- CSS -->
+<style>
+
+</style>
